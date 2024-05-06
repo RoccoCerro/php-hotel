f3f481fa1391acb6598650653cc085953d62e2a0
RoccoCerro <rcerro02@gmail.com>
2024-05-06 15:02:23 +0200

    Add: print date available

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+
+$hotels = [
+
+  [
+    'name' => 'Hotel Belvedere',
+    'description' => 'Hotel Belvedere Descrizione',
+    'parking' => true,
+    'vote' => 4,
+    'distance_to_center' => 10.4
+  ],
+  [
+    'name' => 'Hotel Futuro',
+    'description' => 'Hotel Futuro Descrizione',
+    'parking' => true,
+    'vote' => 2,
+    'distance_to_center' => 2
+  ],
+  [
+    'name' => 'Hotel Rivamare',
+    'description' => 'Hotel Rivamare Descrizione',
+    'parking' => false,
+    'vote' => 1,
+    'distance_to_center' => 1
+  ],
+  [
+    'name' => 'Hotel Bellavista',
+    'description' => 'Hotel Bellavista Descrizione',
+    'parking' => false,
+    'vote' => 5,
+    'distance_to_center' => 5.5
+  ],
+  [
+    'name' => 'Hotel Milano',
+    'description' => 'Hotel Milano Descrizione',
+    'parking' => true,
+    'vote' => 2,
+    'distance_to_center' => 50
+  ],
+
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <main>
+    <ul>
+      <?php
+      foreach($hotels as $hotel){
+        $parking = $hotel['parking'] ? 'Con parcheggio': 'Senza parcheggio';
+
+        echo "
+        <ul>
+          <li>Nome: ".$hotel['name']."</li>
+          <li>Descrizione: ".$hotel['description']."</li>
+          <li>".$parking."</li>
+          <li>Voto: ".$hotel['vote']."</li>
+          <li>Distanza dal centro: ".$hotel['distance_to_center']."</li>
+        </ul>
+        ";
+      };
+      ?>
+    </ul>
+  </main>
+</body>
+</html>
